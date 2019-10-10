3c527d6d390fbf2133f0311550de3dfaac129075
Russel <russel.tsang@gmail.com>
2019-10-09 23:41:37 -0400

    refactor code

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -1,11 +1,11 @@
 class DOMNodeCollection {
   constructor (array) {
-    this.array = array;
+    this.collection = array;
   }
 
   children() {
     let nursery = [];
-    this.array.forEach(el => {
+    this.collection.forEach(el => {
       Array.from(el.children).forEach(child => nursery.push(child));
     });
     return new DOMNodeCollection(nursery);
@@ -13,7 +13,7 @@ class DOMNodeCollection {
 
   parent() {
     let parents = [];
-    this.array.forEach( (el) => {
+    this.collection.forEach( (el) => {
       if (!parents.includes(el.parentElement)) parents.push(el.parentElement);
     });
 
@@ -22,7 +22,7 @@ class DOMNodeCollection {
 
   find(ele) {
     let selected = [];
-    this.array.forEach((el) => {
+    this.collection.forEach((el) => {
       Array.from(el.querySelectorAll(ele)).forEach( (found) => {
         if (!selected.includes(found)) {
           selected.push(found);
@@ -35,39 +35,39 @@ class DOMNodeCollection {
 
   html(string) {
     if (!string) {
-      return this.array[0].innerHTML;
+      return this.collection[0].innerHTML;
     } else {
-      this.array.forEach( function (el){
+      this.collection.forEach( function (el){
         el.innerHTML = string;
       })
     }
   }
 
   empty() {
-    this.array.forEach(function(el) {
+    this.collection.forEach(function(el) {
       el.innerHTML = "";
     })
   }
 
   remove() {
-    this.array.forEach( (el) => el.outerHTML = '')
+    this.collection.forEach( (el) => el.outerHTML = '')
   }
 
   on(action, callback) {
     this.action = action;
-    this.array.forEach( el => {
+    this.collection.forEach( el => {
       el.addEventListener(`${action}`, callback);
     });
   }
 
   append(arg) {
     if (typeof arg === 'string') {
-      this.array.forEach( (el) => {
+      this.collection.forEach( (el) => {
         el.innerHTML += arg; 
       })
     } else if (typeof arg === 'object') {
       arg.array.forEach( (el) => {
-        this.array.forEach((thisArr) => {
+        this.collection.forEach((thisArr) => {
           thisArr.innerHTML += el.outerHTML;
         });
       });
@@ -75,26 +75,26 @@ class DOMNodeCollection {
   }
 
   attr(prop, val) {
-    if (!val) return this.array[0].attributes[prop].value;
-    this.array.forEach( (el) => {
+    if (!val) return this.collection[0].attributes[prop].value;
+    this.collection.forEach( (el) => {
       el.setAttribute(`${prop}`, val);
     })
   }
 
   addClass(name) {
-    this.array.forEach( (el) => {
+    this.collection.forEach( (el) => {
       el.className = name;
     })
   }
 
   removeClass(classesString) {
     if (!classesString) {
-      this.array.forEach( (el) => {
+      this.collection.forEach( (el) => {
         el.className = '';
       });
     } else {
       let classesArr = classesString.split(" ");
-      this.array.forEach( (el) => {
+      this.collection.forEach( (el) => {
         el.className = el.className.split(" ").filter( name => !classesArr.includes(name) ).join(" "); 
       });
     }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 const DOMNodeCollection = require("./dom_node_collection.js");
 
-window.$l = function(el) {
+window.$l = function(ele) {
+  // initialize innerText if the variable is later necessary
   let innerText;
-  if (el[0] === "<") {
-    el = el.slice(1, el.length - 1);
-    if (el.includes('<')) {
-      let start = el.indexOf('>') + 1;
-      let end = el.indexOf('<');
-      innerText = el.slice(start, end);
-      el = el.slice(0, start - 1);
+  // check if user wants to create element
+  if (ele[0] === "<") {
+    ele = ele.slice(1, ele.length - 1);
+    // if format of argument is <element>innerText</element>, 
+    if (ele.includes('<')) {
+      let start = ele.indexOf('>') + 1, 
+          end = ele.indexOf('<');
+
+      // then set innerText variable to the text between tags
+      innerText = ele.slice(start, end);
+      ele = ele.slice(0, start - 1);
     }
-    let x = document.createElement(el);
-    if (innerText) x.innerHTML = innerText;
-    let array = [];
-    array.push(x);
-    return new DOMNodeCollection(array);
+    let newElement = document.createElement(ele);
+    if (innerText) newElement.innerHTML = innerText;
+    return new DOMNodeCollection([newElement]);
+  // else, the user is selecting existing element from DOM, so return node collection
   } else {
-    return new DOMNodeCollection(Array.from(document.querySelectorAll(el)));
+    return new DOMNodeCollection(Array.from(document.querySelectorAll(ele)));
   }
 }
